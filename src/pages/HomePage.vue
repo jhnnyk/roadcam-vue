3195b77c3c751d56b650660d09b838b0982f9991
John Knotts <jhnnyk@gmail.com>
2021-01-13 13:04:31 -0700

    successful db connection

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,7 +5,15 @@
 </template>
 
 <script>
+import { db } from '../firebase';
+
 export default {
   name: 'HomePage',
+  async created() {
+    const data = await db.collection('cams').get();
+    data.forEach((doc) => {
+      console.log(doc.id, ' => ', doc.data());
+    });
+  },
 };
 </script>
